be9dc04f21f11696a97c335553c0e3645671b5b4
mvalente <miguelvalente@protonmail.com>
2024-06-08 12:07:44 +0200

    more handlers

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
+	"unicode"
 )
 
 type apiConfig struct {
@@ -10,6 +14,10 @@ type apiConfig struct {
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.WriteHeader(200)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
 	w.Write([]byte("OK"))
@@ -18,11 +26,15 @@ func handler(w http.ResponseWriter, req *http.Request) {
 func (cfg *apiConfig) handlerHits(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
-	fmt.Println("lol", cfg.fileserverHits)
 	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
 }
 
 func (cfg *apiConfig) handlerResets(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.WriteHeader(200)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
 	cfg.fileserverHits = 0
@@ -31,26 +43,129 @@ func (cfg *apiConfig) handlerResets(w http.ResponseWriter, req *http.Request) {
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits += 1
-		fmt.Println("midle", cfg.fileserverHits)
 		next.ServeHTTP(w, r)
 	})
 }
 
+func (cfg *apiConfig) handlerAdmin(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "text/html") // normal header
+	w.Write([]byte(fmt.Sprintf(`<html>
+
+<body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+</body>
+
+</html>`, cfg.fileserverHits)))
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) http.ResponseWriter {
+	w.WriteHeader(code)
+	errResp := struct {
+		Error string `json:"error"`
+	}{
+		Error: msg,
+	}
+	data, _ := json.Marshal(errResp)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+
+	return w
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) http.ResponseWriter {
+	w.WriteHeader(code)
+	data, _ := json.Marshal(payload)
+	w.Write(data)
+
+	return w
+}
+
+type Chirp struct {
+	Id   int    `json:"id"`
+	Body string `json:"body"`
+}
+
+type ChirpStorage struct {
+	Chirps []Chirp
+}
+
+func (cfg *apiConfig) handlerChirp(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		w = respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		w = respondWithError(w, 400, "Chirp is too long")
+		return
+	} else {
+		cleanedBody := cleanBody(params.Body)
+		w = respondWithJSON(w, 200, Chirp{
+			Id:   len(ChirpStorage.Chirps) + 1,
+			Body: cleanedBody,
+		})
+	}
+}
+
+var badWords = []string{
+	"kerfuffle",
+	"sharbert",
+	"fornax",
+}
+
+func cleanBody(s string) string {
+	for _, badWord := range badWords {
+		s = strings.ReplaceAll(s, badWord, "****")
+		s = strings.ReplaceAll(s, capitalizeFirstLetter(badWord), "****")
+
+	}
+	return s
+}
+func capitalizeFirstLetter(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	runes := []rune(s)
+	runes[0] = unicode.ToUpper(runes[0])
+	return string(runes)
+}
+
 func main() {
 	serverMux := http.NewServeMux()
-	// apiCfg := apiConfig{}
-	apiCfg := new(apiConfig) // automatically initializes fileserverHits to 0
+	apiCfg := new(apiConfig)
 
 	serverMux.Handle("/app/*", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	serverMux.Handle("/assets", http.FileServer(http.Dir("assets/")))
-	serverMux.HandleFunc("/healthz", handler)
-	serverMux.HandleFunc("/metrics", apiCfg.handlerHits)
-	serverMux.HandleFunc("/reset", apiCfg.handlerResets)
+	serverMux.HandleFunc("/api/healthz", handler)
+	serverMux.HandleFunc("/api/metrics", apiCfg.handlerHits)
+	serverMux.HandleFunc("/admin/metrics", apiCfg.handlerAdmin)
+	serverMux.HandleFunc("/api/reset", apiCfg.handlerResets)
+	serverMux.HandleFunc("/api/chirp", apiCfg.handlerChirp)
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serverMux,
 	}
 
-	server.ListenAndServe()
+	log.Println("Starting server on :8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
